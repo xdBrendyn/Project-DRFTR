e6a30b79a2b8f03560f48fa250e3fe44525dbf4e
Brendyn <brendyn1803@gmail.com>
2024-04-22 19:16:31 +0100

    Initial Commit

---
+++ DRFTR/Assets/Scripts/Car.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Car", menuName = "Resources/Car")]
+public class Car : ScriptableObject
+{
+    public enum Tier
+    {
+        E,
+        D,
+        C,
+        B,
+        A,
+        S
+    }
+
+    public enum Type {
+        Sedan,
+        Offroad,
+        Sport,
+        Super
+    }
+
+    public Tier tier;
+    public Type type;
+    public string modelName;
+    [Range(0, 10)] public float zeroToSixty;
+    public int topSpeed;
+    public Sprite carGraphic;
+}
---
+++ DRFTR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Car car;
+}
